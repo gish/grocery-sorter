9fb16479ab62219b7da0f22d84ff84ca058fcc5f
Erik Hedberg <erik@hedberg.at>
2023-01-28 12:23:27 +0100

    feat: use one textfield for input and sort

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,14 @@ import referenceList from "./referenceList.json";
 import "./App.css";
 import {
   Button,
-  Checkbox,
   Container,
   CssBaseline,
-  FormControlLabel,
-  Grid,
   TextField,
   Typography,
 } from "@mui/material";
 import copy from "copy-to-clipboard";
 import { ContentCopy } from "@mui/icons-material";
+import { Stack } from "@mui/system";
 
 const fuse = new Fuse(referenceList, { includeScore: true, threshold: 0.5 });
 interface SearchCache {
@@ -47,44 +45,17 @@ const sortList = (unsortedList: string[]) => {
     });
 };
 
-const getMatch = (item: string) => {
-  const match = fuse.search(item);
-  if (match.length === 0) {
-    return "";
-  }
-  return match[0].item;
-};
-
-const getScore = (item: string) => {
-  const match = fuse.search(item);
-  if (match.length === 0) {
-    return 0;
-  }
-  return match[0].score;
-};
-
 function App() {
   const savedList = window.localStorage.getItem("groceryList");
-  const savedDebug = window.localStorage.getItem("debug") === "true";
   const [groceryList, setGroceryList] = useState(savedList ?? "");
-  const [debugEnabled, setDebugEnabled] = useState(savedDebug);
-  const [sortedList, setSortedList] = useState("");
-  const [sortedDebugList, setSortedDebugList] = useState("");
   const onListUpdate = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     window.localStorage.setItem("groceryList", e.target.value);
     setGroceryList(e.target.value);
   };
   const onGroceryListSort = () => {
-    const sortedListDebug = sortedList
-      .split("\n")
-      .map((item) => `${item} (${getMatch(item)}, ${getScore(item)})`)
-      .join("\n");
-    setSortedList(sortList(groceryList.split("\n")).join("\n"));
-    setSortedDebugList(sortedListDebug);
-  };
-  const onDebugToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
-    window.localStorage.setItem("debug", e.target.checked.toString());
-    setDebugEnabled(e.target.checked);
+    const sortedList = sortList(groceryList.split("\n")).join("\n");
+    window.localStorage.setItem("groceryList", sortedList);
+    setGroceryList(sortedList);
   };
   return (
     <>
@@ -93,48 +64,38 @@ function App() {
         <Typography variant="h4" gutterBottom>
           Veckohandling-sortering
         </Typography>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <TextField
-              onChange={onListUpdate}
-              multiline
-              value={groceryList}
-              rows={20}
-              variant="outlined"
-              fullWidth
-            ></TextField>
-            <FormControlLabel
-              control={
-                <Checkbox checked={debugEnabled} onChange={onDebugToggle} />
-              }
-              label="Debug mode on"
-            />
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={onGroceryListSort}
-            >
-              Sortera
-            </Button>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              multiline
-              value={debugEnabled ? sortedDebugList : sortedList}
-              rows={20}
-              variant="outlined"
-              disabled
-              fullWidth
-            ></TextField>
-            <Button
-              variant="outlined"
-              startIcon={<ContentCopy />}
-              onClick={() => copy(sortedList)}
-            >
-              Kopiera
-            </Button>
-          </Grid>
-        </Grid>
+        <TextField
+          margin="normal"
+          onChange={onListUpdate}
+          multiline
+          value={groceryList}
+          rows={20}
+          variant="outlined"
+          fullWidth
+        ></TextField>
+        <Stack spacing={1} direction="row">
+          <Button
+            variant="outlined"
+            startIcon={<ContentCopy />}
+            onClick={() => setGroceryList("")}
+          >
+            Rensa
+          </Button>
+          <Button
+            variant="outlined"
+            startIcon={<ContentCopy />}
+            onClick={() => copy(groceryList)}
+          >
+            Kopiera
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={onGroceryListSort}
+          >
+            Sortera
+          </Button>
+        </Stack>
       </Container>
     </>
   );
